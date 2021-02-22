147b6fb7d232950539d2571a9dfd1c6b01cb173a
carlos <geral@meuppt.pt>
2021-02-22 15:38:34 +0000

    Initial

---
+++ src/OrderStatus.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace WPH\WC;
+
+class OrderStatus {
+
+	public function __construct() {
+
+	}
+
+	public function register(string $name) {}
+
+	protected function list() {}
+
+	protected function css() {}
+}

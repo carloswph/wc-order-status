65c27ac288ec653a75a7c6f708ab9d714da6d1a4
carlos <geral@meuppt.pt>
2021-02-22 16:33:13 +0000

    Basic wrapper and main class created.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # wc-order-status
+
+Adding custom order statuses to the Woocommerce platform could be definitely easier - and I am not talking about installing yet another plugin for dealing with silly tasks. This helper aims to automate and incept some intelligence to the way developers and web designers add, manage, style and even link behaviours and reactions to the custom statuses created.
+
+First and foremost, we established a class to make the painful routine of adding custom statuses a single line of code.
+
+# Installation
+
+No additional plugins, please. This library can be installed using Composer or, if you prefer, just download the main class `OrderStatus` and add to your project.
+
+# Usage
+
+By definition, for adding a new status to WC orders we first need to register such status in Wordpress, and then include the newly added status to the pool where order statuses are saved within Woocommerce. So... why not doing that at once, instead of successive functions and hooks?
--- src/OrderStatus.php
+++ src/OrderStatus.php
@@ -4,13 +4,92 @@ namespace WPH\WC;
 
 class OrderStatus {
 
-	public function __construct() {
+	protected $statusName;
 
+	protected $statusSlug;
+
+	public $backgroundColor, $fontColor;
+
+	public function __construct(string $statusName, array $colors = null) {
+
+		$this->statusName = $statusName;
+		$this->statusSlug = $this->slugify($statusName);
+		
+		add_action('init', array($this, 'register'));
+		add_filter('wc_order_statuses', array($this, 'list'));
+
+		if($colors) {
+
+			$this->backgroundColor = $colors[0];
+			if(isset($colors[1])) {
+				$this->fontColor = $colors[1];
+			} else {
+				$this->fontColor = 'inherit';
+			}
+			
+			add_action('admin_footer', array($this, 'css'));
+		}
 	}
 
-	public function register(string $name) {}
+	public function register() {
 
-	protected function list() {}
+		register_post_status(
+			'wc-' . $this->statusSlug,
+			array(
+				'label'                     => $this->statusName,
+				'public'                    => true,
+				'exclude_from_search'       => false,
+				'show_in_admin_all_list'    => true,
+				'show_in_admin_status_list' => true,
+				'label_count'               => _n_noop( $this->statusName . ' <span class="count">(%s)</span>', $this->statusName . ' <span class="count">(%s)</span>' ),
+			)
+		);
+	}
 
-	protected function css() {}
+	public function list($order_statuses) {
+
+		$new_order_statuses = array();
+
+	    foreach ( $order_statuses as $key => $status ) {
+        	$new_order_statuses[ $key ] = $status;
+        	$new_order_statuses['wc-' . $this->statusSlug] = $this->statusName;
+        }
+
+        return $new_order_statuses;
+	}
+
+	public function css() {
+		
+		?>
+    	<style>
+        
+			.order-status.status-<?php echo $this->statusSlug; ?> {
+            	background: <?php echo $this->backgroundColor; ?>;
+            	color: <?php echo $this->fontColor; ?>;
+        	}
+    	</style>
+    	<?php
+	}
+
+	public function slugify(string $text) {
+
+	    // replace non letter or digits by -
+	    $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+	    // trim
+	    $text = trim($text, '-');
+	    // transliterate
+	    if (function_exists('iconv'))
+	    {
+	        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+	    }
+	    // lowercase
+	    $text = strtolower($text);
+	    // remove unwanted characters
+	    $text = preg_replace('~[^-\w]+~', '', $text);
+	    if (empty($text))
+	    {
+	        return 'n-a';
+	    }
+	    return $text;
+	}
 }
